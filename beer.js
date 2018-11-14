88097111d32e2103765481f6e3850e1bc0c03222
lukihd <lucas.erisset@ynov.com>
2018-11-14 19:52:46 +0100

    inquire module

--- beer.js
+++ beer.js
@@ -1,12 +1,20 @@
 #!/usr/bin/env node
 const command = require("commander")
+const inquirer = require("inquirer")
+
 
 command 
     .version('0.0.1')
-    .option('-w, --world', 'hello ta mere')
-
-command.parse(process.argv)
+    .description('What\'s my beer command is used to find your beer in the brewery API and it had everythings you want !!')
+    .option('-a, --alphabet', 'Search by first letter')
+    .parse(process.argv)
 
 if (command.world)
-    console.log('hello world')
+    inquirer.prompt([{
+        name: "letter",
+        input: "input",
+        message: "Enter the first letter of your beer"
+    }]).then((answers) => {
+        console.log(answers.name)
+    })
 
