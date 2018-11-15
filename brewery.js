5f49212e888363502c40e83ba8fa2333835ed2b3
Casamandine <amandine.charles@ynov.comgit config --global user.email amandine.charles@ynov.com>
2018-11-15 10:47:13 +0100

    update

--- brewery.js
+++ brewery.js
@@ -7,16 +7,21 @@ breweries={}
 
 command
     .version('0.0.1')
-    .option('-i, --id[value]', 'Search by first letter')
+    .option('-i, --id [value]', 'Search by first letter')
     .parse(process.argv)
 
 if(command.id){
-    arg=process.argv.slice(1)
-    console.log(arg)
+    arg = process.argv.slice(3)
     axios.get('https://sandbox-api.brewerydb.com/v2/brewery/'+arg+'/?key=0dc57b04be75a141664eb685134e5c7d')
-        .then((response)=>{
+        .then((response) => {
             breweries = response.data.data
-            console.log(response)
+            console.log(breweries.name)
+            return axios.get('https://sandbox-api.brewerydb.com/v2/brewery/'+arg+'/locations/?key=0dc57b04be75a141664eb685134e5c7d')
+        })
+        .then((response) => {
+            breweries['Locality'] = response.locality
+            breweries['Rergion'] = response.region
+            console.log(breweries)        
         })
         .catch('ERREUR : l\'id'+arg+'n\est pas dans la base de donnée')
 }
