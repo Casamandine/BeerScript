ae2aba06d55cb6437ddd8baef51abcd1296d92ed
lukihd <lucas.erisset@ynov.com>
2018-11-15 10:47:54 +0100

    update beer

--- beer.js
+++ beer.js
@@ -3,7 +3,7 @@ const command = require("commander")
 const inquirer = require("inquirer")
 const axios = require("axios")
 
-bieres = {}
+beers = {}
 
 command 
     .version('0.0.1')
@@ -15,16 +15,24 @@ command
 if (command.id) {
     arg = process.argv.slice(3)
     axios.get('https://sandbox-api.brewerydb.com/v2/beer/' + arg + '/?key=0dc57b04be75a141664eb685134e5c7d/')
-        .then((test) => {
-            bieres = test.data.data
-            console.log(bieres.name)
+        .then((response) => {
+            beer = response.data.data
+            beers['Id'] = beer.id         
+            beers['Name'] = beer.name         
+            beers['Alcohol_by_volume'] = beer.abv 
+            
+            console.log(beers)
         })
         .catch('ERREUR : la bière avec l\'id ' + arg + ', n\'est pas dans la base de donnée')
 }
 else if (command.random) {
     axios.get('https://sandbox-api.brewerydb.com/v2/beer/random/?key=0dc57b04be75a141664eb685134e5c7d/')
     .then((response) => {
-        bieres['name'] = response.data[0].name[0]
-        console.log(bieres)
+        beer = response.data.data
+        beers['Id'] = beer.id         
+        beers['Name'] = beer.name         
+        beers['Alcohol_by_volume'] = beer.abv 
+
+        console.log(beers)
     })
 }
